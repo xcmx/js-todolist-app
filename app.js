e420fb0412d5cef8b8af255efd5c118f999b6e9c
Andrew Apichenok <andrew@talentfly.com>
2023-05-10 11:50:52 +0300

    todolist app add implementation

---
+++ app.js
@@ -0,0 +1,195 @@
+// Define UI Vars
+
+const form = document.querySelector('.create-task-form');
+const filter = document.querySelector('.filter-input');
+const taskInput = document.querySelector('.task-input');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // DOM Load event
+  document.addEventListener('DOMContentLoaded', getTasks);
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Edit task event
+  taskList.addEventListener('click', editTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// Get Tasks from LS
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task){
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    const editButton = document.createElement('span');
+    editButton.className = 'edit-item secondary-content';
+    editButton.innerHTML = '<i class="fa fa-edit"></i>';
+    li.appendChild(editButton);
+    // Create new link element
+    const removeButton = document.createElement('span');
+    removeButton.className = 'delete-item secondary-content';
+    removeButton.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(removeButton);
+
+    // Append li to ul
+    taskList.appendChild(li);
+  });
+}
+
+// Add Task
+function addTask(e) {
+  if(taskInput.value.trim() === '') {
+     e.preventDefault();
+    return null;
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element to edit item
+  const editButton = document.createElement('span');
+  editButton.className = 'edit-item secondary-content';
+  editButton.innerHTML = '<i class="fa fa-edit"></i>';
+  li.appendChild(editButton);
+  // Create new link element to remove item
+  const removeButton = document.createElement('span');
+  removeButton.className = 'delete-item secondary-content';
+  removeButton.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(removeButton);
+
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Store in LS
+  storeTaskInLocalStorage(taskInput.value);
+
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+// Edit task
+
+function editTask(e) {
+  if (e.target.parentElement.classList.contains('edit-item')) {
+    let currentTask = e.target.parentElement.parentElement.childNodes[0].textContent;
+    let editedTask = window.prompt("Please type a new name", currentTask);
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    if (editedTask && (editedTask !== currentTask)) {
+      taskIndex = tasks.indexOf(currentTask);
+      e.target.parentElement.parentElement.childNodes[0].textContent = editedTask;
+      tasks[taskIndex] = editedTask;
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+  }
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Ви впевнені що хочете видалити це завдання?')) {
+      e.target.parentElement.parentElement.remove();
+
+      // Remove from LS
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+    }
+  }
+}
+
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task){
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  if (confirm('Ви впевнені що хочете видалити ВСІ завдання?')) {
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+    }
+  }
+
+  // Clear from LS
+  clearTasksFromLocalStorage();
+}
+
+// Clear Tasks from LS
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().includes(text.toLowerCase())){
+      task.style.display = 'flex';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
